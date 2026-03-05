c4d210b96c06d823a7ffce8252bda1aa9f33104c
Ingrid Larissa <larissaorcx@gmail.com>
2026-03-04 23:46:29 -0300

    feat: novas rotas

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,6 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx"), route("title", "routes/title.tsx") ] satisfies RouteConfig;
-
+export default [
+  index("routes/home.tsx"),
+  route("title", "routes/title.tsx"),
+] satisfies RouteConfig;
